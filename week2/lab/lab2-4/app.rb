c18c478147c98b4e4198b83131b7b95c8b2d170a
vader1359 <vader1359@gmail.com>
2017-07-20 13:40:55 +0700

    Finish Lab 2.4 milestone-a

--- week2/lab/lab2-4/app.rb
+++ week2/lab/lab2-4/app.rb
@@ -1,7 +1,25 @@
 require "bundler/setup"
 Bundler.require
+@@items = [
+    {name: "Learn Form element", status: "done"},
+    {name: "Learn Sinatra params", status: "undone"}
+  ]
 
 get "/" do
-  
-  erb :"index.html"
+  erb :"index.html", locals: {items: @@items}
 end
+
+# This means go to the page /submit and add the parameter {name}
+post "/submit" do 
+
+  p params
+  new_item = {name: params["item_name"], status: "undone"}
+  p @@items
+  @@items << new_item
+  p @@items
+
+  erb :"index.html", locals: {items: @@items}
+
+end
+
+
