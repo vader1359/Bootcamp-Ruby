7bedf5946d244eaac270c8e623855fc5f55915a6
vader1359 <vader1359@gmail.com>
2017-07-20 14:42:17 +0700

    Finish Milstone=B

--- week2/lab/lab2-4/app.rb
+++ week2/lab/lab2-4/app.rb
@@ -1,25 +1,36 @@
 require "bundler/setup"
 Bundler.require
-@@items = [
-    {name: "Learn Form element", status: "done"},
-    {name: "Learn Sinatra params", status: "undone"}
-  ]
 
 get "/" do
-  erb :"index.html", locals: {items: @@items}
+  
+  lines = File.read("todo.data").split("\n")
+  items = lines.map do |line|
+    status = line[3] == "x" ? "done" : "undone"
+    name = line[6..-1]
+    #
+    item = {name: name, status: status}
+    
+  end
+  
+  erb :"index.html", locals: {items: items}
 end
 
 # This means go to the page /submit and add the parameter {name}
 post "/submit" do 
-
+  
   p params
+  variables = params
   new_item = {name: params["item_name"], status: "undone"}
-  p @@items
-  @@items << new_item
-  p @@items
-
-  erb :"index.html", locals: {items: @@items}
-
+  
+  
+  File.open("todo.data", "a") do |f|
+    f << "\n"
+    f << "- [ ] " + new_item[:name]
+  end
+  
+  
+  redirect to("/")
+  
 end
 
 
