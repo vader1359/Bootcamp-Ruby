480b5b762ea714c3317996baba3d02f6be404ed6
vader1359 <vader1359@gmail.com>
2017-07-23 10:58:54 +0700

    Save before dealing with index

--- week2/assignment/app.rb
+++ week2/assignment/app.rb
@@ -1,6 +1,20 @@
+require_relative "item"
+require_relative "list"
+require_relative "todo"
+
 require "Bundler/setup"
 Bundler.require
 
+
+
 get "/" do
-  erb :"index.html"
+  p todo = Todo.new.show_all
+  
+  erb :"index.html", layout: :"layout.html", locals: {todo: todo}
+end
+
+post "/change_status" do
+  puts params
+
+  redirect to("/")
 end
